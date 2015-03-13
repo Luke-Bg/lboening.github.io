9fea3199d36abcc094d3a012000cccb8f1af658b
Luke Boening <luke.boening@me.com>
2015-03-12 19:51:39 -0500

    Added minor change to help
    Changes to help

--- LBHelpers.ps1
+++ LBHelpers.ps1
@@ -1,4 +1,11 @@
-﻿Function Add-ConsoleApp{
+﻿## Script Name: LBHelpers.ps1
+## Purpose: Helper functions
+## Record Content Author: luke_2
+## Revision: 0.1
+## Create date: 03/12/2015 19:40:51
+## Last update: 03/12/2015 19:40:51
+
+Function Add-ConsoleApp{
 [cmdletbinding()]
 param(
 [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=0)]
@@ -19,23 +26,26 @@ Add-Type -OutputType ConsoleApplication -OutputAssembly HelloWorld.exe $assembly
 }
 }
 
+
+
 Function Get-Links {
 [cmdletbinding()]
 param(
-[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
-[string]$url="https://gsa.github.io/data/dotgov-domains/2014-12-01-full.csv"
+[Parameter(Mandatory=$false,ValueFromPipeLine=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+[string]$url="https://gsa.github.io/data/dotgov-domains/2014-12-01-full.csv",
+[switch]$version
 )
+begin { if($version) { "Get-Links version {0}" -f ("0.1.1") } }
 process{
  $wc = New-object System.Net.Webclient
- $resp = $wc.DownloadString($url)
- return $resp
+ $wc.DownloadString($url)
 }
 }
 
 
 <#
 .Synopsis
-   Get-WebHeaders sends head request to URI and returns a Powershell custom object
+   Get-WebHeaders sends head request to URI and returns a Powershell custom object. Revision 0.1.3 2015-03-12
 .DESCRIPTION
    Gets header, returns object with following properties
      KeyToRec: Optional key, used for JSON
@@ -45,7 +55,13 @@ process{
      DateTimeUTC: The datetime the command was run
      You can use these commands for further processing
 	 Example output:
-		blah
+       Error       : False
+       Uri         : http://ABINGDON-VA.GOV/
+       KeyToRec    : 151558
+       DateTimeUTC : 2015-03-12T19:42:47.3972921-05:00
+       StatusCode  : 200
+       Headers     : {[Vary, Accept-Encoding], [Connection, close], [Content-Length, 26937], [Content-Type, text/html]...}
+		
 .EXAMPLE
    Get-Webheaders -uri 'http://ABINGDON-VA.GOV/'
 .EXAMPLE
@@ -54,35 +70,57 @@ process{
    # Examples
    $links = Get-Links -url "https://gsa.github.io/data/dotgov-domains/2014-12-01-full.csv" | Convertfrom-csv
    $links.'Domain Name'[0..5] | % {'http://'+$_.'Domain Name'} | get-webheaders | convertto-json -depth 2 -compress | out-file c:\alldata\responses.json -encoding ascii
+.EXAMPLE
+   # Output 
+   Get-Webheaders -uri 'http://ABINGDON-VA.GOV/'
+        Error       : False
+        Uri         : http://ABINGDON-VA.GOV/
+        KeyToRec    : 151558
+        DateTimeUTC : 2015-03-12T19:42:47.3972921-05:00
+        StatusCode  : 200
+        Headers     : {[Vary, Accept-Encoding], [Connection, close], [Content-Length, 26937], [Content-Type, text/html]...}
 .PARAMETER  Uri
    Enter the URI to the resource
+   Type: string
+   Purpose: invokes the uri
+   Validation: checks for http or https
+   Example(s): http://github.io, http://VA.GOV
 .PARAMETER KeyToRec
    Enter an optional key to the record
    Type: string
-   Used for JSON output
+   Purpose: Used for JSON output
+   Default: random number 
    Validation: none
+   Examples(s): KEY1234, WEBHEAD01OF20, 151558
+.PARAMETER Demo
+   Enter switch for demo mode
 .INPUTS
    [string] URI. Validated as HTTP.
    [string] KeyToRec (optional key added to output)
 .OUTPUTS
-   [psobject] with keys and values
+   [PSCUSTOMOBJECT] with keys and values representing the headers from the web response
 .LINK
    http://lboening.github.io/LBHelpers.psm1
    https://msdn.microsoft.com/en-us/library/hh847834.aspx   
 .NOTES
    # Lifecycle
-
-   
-   # Test
-   # Copy-item .\Lbhelpers.ps1 -destination .\Tests
    # CD .\Tests
+   # Edit .\LBHelpers.ps1
    # Invoke-Pester
+   # Copy-item .\LBHelpers.ps1 ..\.\lboening.github.io
 
+   # Create manifest
    # New-ModuleManifest -NestedModules ".\Lbhelpers.psm1" -Author "Luke Boening" -CompanyName "Luke Boening" -Copyright "None" -Description "Testing module creation" -ModuleVersion "0.1.1" -path "C:\Code\scripts\LBHelpers.psd1" -RootModule ".\LbHelpers.psm1"  -PowerShellVersion 3.0 -Confirm
-   # Test-ModuleManifext -Path c:\code\scripts\LBHelpers.psd1
+   # Test-ModuleManifest -Path c:\code\scripts\LBHelpers.psd1
    
    # Deploy
-   # copy-item .\LBHelpers.ps* ..\lboening.github.io\LBHelpers.ps*
+   # copy-item .\LBHelpers.ps* ..\lboening.github.io
+   # Git commit
+   # Git push
+
+   # Edit web page and deploy
+   # Update README.MD
+   # Update INDEX.HTML
    # Git commit
    # Git push
 
@@ -96,12 +134,14 @@ Function Get-WebHeaders{
 [cmdletbinding()]
 [Outputtype([psobject])]
 param(
-[parameter(Mandatory=$true,ValueFromPipeLine=$true)]
+[switch]$version,
+[parameter(Mandatory=$false,ValueFromPipeLine=$true)]
 [ValidatePattern('http*')]
-[string]$uri,
+[string]$uri="http://ABINGDON-VA.GOV/",
 [parameter(Mandatory=$false,ValueFromPipeLine=$true)]
 [string]$keytorec
 )
+begin { if ($version) { "Get-Webheaders version: {0}" -f ("0.1.4") } }
 Process {
    try {
     if (!($keytorec)) { $keytorec = (Get-random -SetSeed 1000 -maximum 1000000 -minimum 1) }
@@ -120,3 +160,14 @@ Process {
 
 ## New-ModuleManifest -NestedModules ".\Lbhelpers.psm1" -Author "Luke Boening" -CompanyName "Luke Boening" -Copyright "None" -Description "Testing module creation" -ModuleVersion "0.2.0" -path "C:\USERS\LUKE_2\Documents\WindowsPowershell\Modules\LBHelpers\LBHelpers.psd1" -RootModule ".\LbHelpers.psm1" -confirm
 
+# invoke-pester
+
+$hv = @"
+## Script Name:
+## Purpose: 
+## Record Content Author: $($env:USERNAME)
+## Revision: 0.1
+## Create date: $(get-date)
+## Last update: $(get-date)
+"@
+new-IseSnippet -Title "Set Comment Header" -Description "Set comment header" -text "$hv" -CaretOffset 15 -Author "Luke Boening" -Force
